6d2c07f4da80e3501574dc5e078b265931523fa4
Gabriela Lena <gabriela.lena@outlook.com>
2023-01-10 12:44:13 -0300

    Adiciona Dtos

---
+++ BancoNorton.DAL/DTO/ClienteDTO.cs
@@ -0,0 +1,22 @@
+﻿using BancoNorton.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoNorton.DAL.DTO
+{
+    public class ClienteDTO
+    {
+        [Required(ErrorMessage = "O CPF é obrigatório.")]
+        [Range(11, 11, ErrorMessage = "São necessários 11 caracteres para o Cpf.")]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [MaxLength(50, ErrorMessage = "O nome não pode exceder 50 caracteres.")]
+        public string Nome { get; set; }
+        public ContaDTO Conta { get; set; }// para criar um cliente, uma conta sera criada juntamente
+    }
+}
---
+++ BancoNorton.DAL/DTO/ContaDTO.cs
@@ -0,0 +1,20 @@
+﻿using BancoNorton.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoNorton.DAL.DTO
+{
+    public class ContaDTO
+    {
+        [Required(ErrorMessage = "O número de conta é obrigatório.")]
+        public int NumeroConta { get; set; }
+
+        [Required(ErrorMessage = "O valor de saldo é obrigatório.")]
+        public int Saldo { get; set; }
+        public Cliente? Cliente { get; set; }// para criar uma conta, por id de cliente já existente
+    }
+}
